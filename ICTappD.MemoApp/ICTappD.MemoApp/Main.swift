283ec465f7b00fc3250ed0cdfe4fa28b1e69c2d1
SeanSangkyuLee <21000489@handong.edu>
2018-06-01 14:13:07 +0900

    Working

--- ICTappD.MemoApp/ICTappD.MemoApp/Main.swift
+++ ICTappD.MemoApp/ICTappD.MemoApp/Main.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class Main: UIViewController {
+class Main: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    var MemoData = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        MemoData = UserDefaults.standard.object(forKey: "MemoData") as? [String] ?? [String]()
+        
+        return MemoData.count
+        //for each section how many rows?
+        //Flexible numbers
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let Cell = tableView .dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        //Design of each Cells
+        //let cell, reusable cells and Identifier makes to name it
+        return Cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let MemoNumber = indexPath.row
+        UserDefaults.standard.set(MemoNumber, forKey: "MemoNumber")
+        //This code is for saving
+        //Therefore if we click it, check the index number
+        //Save the memonumber and get the record number -> Record face
+        
+        //show the number to change the showing status depending on the row
+        //For each rows, we call it row
+        
+        self.performSegue(withIdentifier: "ToRecord", sender: self)
+    
+        //When select the row what are you gonna do?
+        //show the record scene
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Add(_ sender: Any) {
+        
+        UserDefaults.standard.set(-1, forKey: "MemoNumber")
+        
+    }
+    
+    
 }
 
--- ICTappD.MemoApp/ICTappD.MemoApp/Record.swift
+++ ICTappD.MemoApp/ICTappD.MemoApp/Record.swift
@@ -9,13 +9,105 @@
 import UIKit
 
 class Record: UIViewController {
-
+    // Record is the name, UIviewController is the type
+    @IBOutlet weak var RecordTextView: UITextView!
+    var MemoData = [String]()
+    
+    //let does not change
+    //var does change
+    //Group which is consist of String
+    //outlet means just show
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let MemoNumber = UserDefaults.standard.object(forKey: "MemoNumber") as! Int
+        //First, get the MemoNumber, this is the number since there are numbers on the list.
+        
+        if MemoNumber == -1 {
+            
+            MemoData = UserDefaults.standard.object(forKey: "MemoData") as! [String]	
+            RecordTextView.text = "..."
+            
+            
+        } else {
+            MemoData = UserDefaults.standard.object(forKey: "MemoData") as! [String]
+            //Why? Because there is always data in here
+            
+            RecordTextView.text = MemoData[MemoNumber]
+            //show the things that in the Main's number
+            
+        }
+        
+     
+    }
+    //This is for adding the data
+    
+    
+    
+    
+/*
+        if MemoData.count == 0{
+            RecordTextView.text = "..."
+            //if there is no data, just erase
+            
+        } else {
+            let MemoNumber = UserDefaults.standard.object(forKey: "MemoNumber") as! Int
+            //So MemoNumber is always been saved
+            
+            RecordTextView.text = MemoData[MemoNumber]
+        }
+        
+        
+        
 
         // Do any additional setup after loading the view.
+        //when view is loaded what should we do?
+        //So, It can be string or not therefore, you have to put ? on the back of as
+        //save it as a name "0" and call it
+        //When calling the memodata, string, if there is no string just make it.
+        
+    
     }
-
+*/
+    
+    
+    
+    @IBAction func Save(_ sender: Any) {
+        
+       let MemoNumber = UserDefaults.standard.object(forKey: "MemoNumber") as! Int
+        
+        if MemoNumber == -1 {
+            
+            MemoData.insert(RecordTextView.text, at: 0)
+            
+            UserDefaults.standard.set(MemoData, forKey: "MemoData")
+            
+        } else {
+            
+            MemoData.remove(at: MemoNumber)
+            MemoData.insert(RecordTextView.text, at: MemoNumber)
+            
+            UserDefaults.standard.set(MemoData, forKey: "MemoData")
+            
+            
+        }
+      
+    }
+    //action means do something
+    //This is adding Memos, insert it and put it
+    //at means put the new memo on the top of the list
+    //Since we are saving the MemoData itself, we put MemoData, and name is MemoData
+    //We have to check whether it came from the add or from the list
+    
+    //so erase it, change it and put it in the same place
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
